0451c386af605fa34a85649400106a4765fa2c80
Tim Rekelj <tim.rekelj@gmail.com>
2022-01-09 02:23:35 +0100

    Total rework of website

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,128 @@
 <script>
-	export let name;
+	import Button from './Button.svelte';
+	let dark = false;
+
+	function handleSubmit() {
+		dark = !dark;
+	}
+
 </script>
 
+<svelte:head>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+</svelte:head>
+
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<div class="toggle-darkmode">
+
+		<Button bind:value={dark} on:submit={handleSubmit}>
+
+			{#if dark }
+				<i class="material-icons">light_mode</i>
+			{:else }
+				<i class="material-icons">nightlight</i>
+			{/if}
+
+		</Button>
+	</div>
+	<div class="title-box">
+		<div class="center-help">
+			<h1>"Website comming soon"</h1>
+			<p>~ Tim Rekelj, 2022</p>
+		</div>
+	</div>
+	<div class="footer">
+		<div class="quote">"Bears, beets, battlestar galactica"</div>
+		<div class="icons">
+			<img src="icons/github.svg" alt="github" height="50px">
+			<img src="icons/instagram.svg" alt="instagram" height="50px">
+			<img src="icons/twitter.svg" alt="twitter" height="50px">
+			<img src="icons/spotify.svg" alt="spotify" height="50px">
+		</div>
+	</div>
 </main>
 
 <style>
 	main {
+		height: 100%;
 		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.toggle-darkmode {
+		position: absolute;
+		top: 40px;
+		right: 40px;
+	}
+
+	.title-box {
+		background-color: #282627;
+		width: 800px;
+		height: 300px;
+		display: flex;
+		justify-content: center;
+		box-shadow: 0 0 40px #282627;
+		border-radius: 10px;
+	}
+
+	.center-help {
+		padding: 0;
+		margin: auto 0;
+	}
+
+	.footer {
+		position: absolute;
+		bottom: 40px;
+		width: 93%;
+		padding: 10px 20px;
+		background-color: #282627;
+		box-shadow: 0 0 40px #282627;
+		border-radius: 10px;
+		display: flex;
+		align-items: center;
+	}
+
+	.quote {
+		color: #EEF0F2;
+		float: left;
+		font-size: 1.5em;
+	}
+
+	.icons {
+		margin: 0 0 0 auto;
+		order: 2;
+	}
+
+	img {
+		margin: 10px;
 	}
 
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
+		color: #EEF0F2;
 		font-size: 4em;
-		font-weight: 100;
+		padding: 0;
+		margin: 0;
 	}
 
-	@media (min-width: 640px) {
+	p {
+		color: #EEF0F2;
+		padding: 0;
+		margin: 0;
+		float: right;
+	}
+
+	:global(body) {
+		background-color: #EEF0F2;
+		transition: background-color 0.3s
+	}
+	:global(body.dark-mode) {
+		background-color: #403B3D;
+	}
+
+	@media (min-width: 940px) {
 		main {
 			max-width: none;
 		}
---
+++ src/Button.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+    const dispatch = createEventDispatcher();
+
+    function toggle() {
+        window.document.body.classList.toggle('dark-mode');
+        dispatch('submit');
+    }
+</script>
+<button on:click={toggle}>
+    <slot/>
+</button>
+<style>
+    button {
+        background-color: #282627;
+        color: #EEF0F2;
+        border-radius: 10px;
+        padding: 20px;
+        border: none;
+		box-shadow: 0 0 40px #282627;
+        
+    }
+    :global(body.dark-mode) button {
+        background-color: #282627;
+        color: #EEF0F2;
+    }
+</style>
