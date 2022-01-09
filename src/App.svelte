23fee912914821d7af222b14b8db2cc0b5b20d05
Tim Rekelj <tim.rekelj@gmail.com>
2022-01-10 00:32:36 +0100

    Added links to icons

--- src/App.svelte
+++ src/App.svelte
@@ -83,10 +83,10 @@
 					{/if}
 				</Button>
 			</div>
-			<img src="icons/github.svg" alt="github" height="50px">
-			<img src="icons/instagram.svg" alt="instagram" height="50px">
-			<img src="icons/twitter.svg" alt="twitter" height="50px">
-			<img src="icons/spotify.svg" alt="spotify" height="50px">
+			<a href="https://github.com/timrekelj"><img src="icons/github.svg" alt="github" height="50px"></a>
+			<a href="https://www.instagram.com/timrekelj/"><img src="icons/instagram.svg" alt="instagram" height="50px"></a>
+			<a href="https://twitter.com/timrekelj"><img src="icons/twitter.svg" alt="twitter" height="50px"></a>
+			<a href="https://open.spotify.com/user/9nksd7kzg4meppj2r4kg6cldg?si=fce19c5d8fb34c31"><img src="icons/spotify.svg" alt="spotify" height="50px"></a>
 		</div>
 	</div>
 </main>
